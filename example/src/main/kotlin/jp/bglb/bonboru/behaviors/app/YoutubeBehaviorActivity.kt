f6e2b4d9fff3f487556f419f369e4ca1386eac8e
ttymsd <ttymsd@gmail.com>
2017-03-31 15:18:50 +0900

    replace header

--- example/src/main/kotlin/jp/bglb/bonboru/behaviors/app/YoutubeBehaviorActivity.kt
+++ example/src/main/kotlin/jp/bglb/bonboru/behaviors/app/YoutubeBehaviorActivity.kt
@@ -10,8 +10,20 @@ import butterknife.bindView
 import jp.bglb.bonboru.behaviors.YoutubeLikeBehavior
 
 /**
- * Created by tetsuya on 2017/01/05.
- */
+* Copyright (C) 2017 Tetsuya Masuda
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 class YoutubeBehaviorActivity() : AppCompatActivity(), YoutubeLikeBehavior.OnBehaviorStateListener {
   val root by bindView<CoordinatorLayout>(R.id.root)
   val show by bindView<Button>(R.id.show)
