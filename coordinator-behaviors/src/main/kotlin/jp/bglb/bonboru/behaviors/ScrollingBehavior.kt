a8fdea4f300667694241cb4bd059cd70bcf5622e
ttymsd <ttymsd@gmail.com>
2017-04-02 15:30:10 +0900

    refactoring

--- coordinator-behaviors/src/main/kotlin/jp/bglb/bonboru/behaviors/ScrollingBehavior.kt
+++ coordinator-behaviors/src/main/kotlin/jp/bglb/bonboru/behaviors/ScrollingBehavior.kt
@@ -6,24 +6,24 @@ import android.util.AttributeSet
 import android.view.View
 
 /**
-* Copyright (C) 2017 Tetsuya Masuda
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-class ScrollingBehavior<V : View>(context: Context?, attrs: AttributeSet?) : CoordinatorLayout.Behavior<V>(
+ * Copyright (C) 2017 Tetsuya Masuda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class ScrollingBehavior<V : View>(context: Context,
+    attrs: AttributeSet?) : CoordinatorLayout.Behavior<V>(
     context, attrs) {
 
-  var initialized = false
   var peekHeight = 300
   var anchorPointY = 600
   var currentChildY = 0
@@ -31,12 +31,12 @@ class ScrollingBehavior<V : View>(context: Context?, attrs: AttributeSet?) : Coo
 
   init {
     attrs?.let {
-      val typedArray = context?.obtainStyledAttributes(it, R.styleable.GoogleMapLikeBehaviorParam)
-      peekHeight = typedArray?.getDimensionPixelSize(
-          R.styleable.GoogleMapLikeBehaviorParam_peekHeight, 0)!!
-      anchorTopMargin = typedArray?.getDimensionPixelSize(
-          R.styleable.GoogleMapLikeBehaviorParam_anchorPoint, 0)!!
-      typedArray?.recycle()
+      val typedArray = context.obtainStyledAttributes(it, R.styleable.GoogleMapLikeBehaviorParam)
+      peekHeight = typedArray.getDimensionPixelSize(
+          R.styleable.GoogleMapLikeBehaviorParam_peekHeight, 0)
+      anchorTopMargin = typedArray.getDimensionPixelSize(
+          R.styleable.GoogleMapLikeBehaviorParam_anchorPoint, 0)
+      typedArray.recycle()
     }
   }
 
